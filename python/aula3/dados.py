4ba63599c080586b625fb30b2a96cb93bff1582e
luiz <luiz.com@gmail.com>
2026-08-12 22:14:17 -0300

    Add initial implementation of product and client data collection in dados.py and dicionario.py

---
+++ python/aula3/dados.py
@@ -0,0 +1,17 @@
+#dados
+
+produto = dict(nome="Notebook", preco=300)
+print(produto["nome"]) 
+
+########################################
+
+cliente - {}
+cliente['nome'] = input("nome")
+cliente['idade'] = int(input("idade"))
+cliente['cidade'] = input("cidade")
+
+print(f"nome: {cliente['nome']}")
+
+#########################################
+
+produto = dict(nome="Notebook", preco=300)
---
+++ python/aula3/dicionario.py
@@ -0,0 +1,13 @@
+dicionario = {
+    'time': ["a", "b", "c"],
+    'vitorias' : [10, 20, 30],
+    'Estados' : ["rj", "sp", "ce"]
+}
+
+print(dicionario)
+
+import pandas as pd
+
+dados = pd.DataFrame(dicionario)
+dados 
+#mostra em planilha
