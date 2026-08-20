fd3d49bcad87231042192ead9abae2761b127d12
luiz <luiz.cerqueira2007@gmail.com>
2026-08-19 22:36:51 -0300

    Correct monitoramento class and method usage
    Fix class definition and method call in Aula4.py

---
+++ python/Aula4.py
@@ -0,0 +1,9 @@
+class monitoramento:
+  def __init__(self, nome, idade):
+    self.nome = nome
+    slef.idade = idade
+  def resultados:
+    return f" O {self.nome} tem {self.idade} anos."
+  
+  p1 = monitoramento
+  print(p1.resultado)
